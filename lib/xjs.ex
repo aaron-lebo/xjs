1d17c1e80ffffb53d8518be3f502986a594d353f
Aaron Lebo <aaron.matthew.lebo@gmail.com>
2016-04-01 02:43:46 +0000

    added not bad

--- lib/xjs.ex
+++ lib/xjs.ex
@@ -1,77 +1,62 @@
 defmodule XJS do
-  defmodule Program do
-    @derive [Poison.Encoder]
-
-    defstruct [:body]
-  end
-
-  def literal(value) do
-    [
+  def compile(value) when
+    is_number(value) or is_bitstring(value) do
+    %{
       type: :Literal,
       value: value
-    ]
-  end
-
-  def compile(node) when
-    is_number(node) do
-    literal node
-  end
-
-  def compile(node) when
-    is_bitstring(node) do
-    literal node
+    }
   end
 
   def compile({:fun, _, tail}) do
     {params, [[do: body]]} = Enum.split tail, -1
-    [
+    %{
       type: "ArrowExpression",
-      params: Enum.map(params, fn x -> compile x end),
+      params: Enum.map(params, fn x -> compile x  end),
       body: compile(body)
-    ]
+    }
   end
 
   def compile({:=, _, [left, right]}) do
-    [
+    %{
       type: :AssignmentExpression,
       operator: :=,
       left: compile(left),
       right: compile(right)
-    ]
+    }
   end
 
   def compile({operator, _, [left, right]}) when
     operator in [:*, :/, :+, :-] do
-    [
+    %{
       type: :BinaryExpression,
       operator: operator,
       left: compile(left),
       right: compile(right)
-    ]
+    }
   end
 
   def compile({kind, _, [{:=, _, [head, body]}]}) when
     kind in [:con, :let] do
-    [
+    %{
       type: :VariableDeclaration,
-      declarations: [
+      declarations: %{
         type: :VariableDeclarator,
         id: compile(head),
         init: compile(body)
-      ],
+      },
       kind: kind == :con && :const || :let
-    ]
+    }
   end
 
   def compile({name, _, nil}) do
-    [
+    %{
       type: :Identifier,
       name: name
-    ]
+    }
   end
 
   def compile(node) do
-    {:error, node}
+    %{error: node}
   end
 
   defmacro xjs(do: block) do
@@ -79,13 +64,12 @@ defmodule XJS do
       {:__block__, _, ast} -> Enum.map ast, &XJS.compile/1
       ast -> compile ast
     end
+    |> Poison.encode!
   end
 
   def test() do
     xjs do
-      fun x, y do
-        x + y
-      end
+      0 + 1
     end
   end
 end
