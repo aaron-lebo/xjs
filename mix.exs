f2b5b1744729990f1005098cd62284b01c84e58b
Aaron Lebo <aaron.matthew.lebo@gmail.com>
2016-04-06 00:15:54 +0000

    added package

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule XJS.Mixfile do
      version: "0.0.1",
      elixir: "~> 1.2",
      description: "elixir syntax, javascript semantics",
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
