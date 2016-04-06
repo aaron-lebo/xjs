731312de7ffd46ab079ff75b0307910e1c41c079
Aaron Lebo <aaron.matthew.lebo@gmail.com>
2016-04-06 00:35:56 +0000

    spelled better

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule XJS.Mixfile do
 
   def project do
     [app: :xjs,
-     version: "0.0.3",
+     version: "0.0.4",
      elixir: "~> 1.2",
      description: "elixir syntax, javascript semantics",
      package: package,
